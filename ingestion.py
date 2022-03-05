3c9dbc757ee0eda7f721966b3342350d20d5a290
Oliver Schurius <mail@schurius.de>
2022-03-06 00:05:53 +0100

    ingestion draft v1

--- ingestion.py
+++ ingestion.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 import json
 from datetime import datetime
-from typing import Optional
+import typing
+
 
 # initialize logging
 import logging 
@@ -22,17 +23,38 @@ input_folder_path = config['input_folder_path']
 output_folder_path = config['output_folder_path']
 
 
+def append_dataset(file_name: str, dataset: pd.DataFrame) -> pd.DataFrame:
+    
+    logger.info(f"File name: {str(file_name)}")            
+    if (file_name[-3:] == 'csv'):
+        logger.info(f"Append csv file: {file_name}")
+        tmp = pd.read_csv(file_name)
+        logger.info(f"Append dataset: {tmp.shape}")
+        appended_dataset = tmp if dataset.empty else dataset.append(tmp)
+    elif (file_name[-4:] == 'json'):
+        logger.info(f"Append json file: {file_name}")
+        tmp = pd.read_json(file_name)
+        logger.info(f"Append dataset: {tmp.shape}")
+        appended_dataset = tmp if dataset.empty else dataset.append(tmp)
+    else:
+        raise UnsupportedFileType("Detected unsupported file type")
+    
+    return appended_dataset
+
+
 def merge_dataset(file_name: str, dataset: pd.DataFrame) -> pd.DataFrame:
     
     logger.info(f"File name: {str(file_name)}")            
     if (file_name[-3:] == 'csv'):
         logger.info(f"Merge csv file: {file_name}")
-        merged_dataset = pd.read_csv(file_name) if dataset.empty else dataset.merge(pd.read_csv(file_name), how='outer')
-        logger.info(f"Merged dataset: {merged_dataset.shape}")
+        tmp = pd.read_csv(file_name)
+        logger.info(f"Merged dataset: {tmp.shape}")
+        merged_dataset = tmp if dataset.empty else dataset.merge(tmp, how='outer')
     elif (file_name[-4:] == 'json'):
         logger.info(f"Merge json file: {file_name}")
-        merged_dataset = pd.read_json(file_name) if dataset.empty else dataset.merge(pd.read_json(file_name), how='outer')
-        logger.info(f"Merged dataset: {merged_dataset.shape}")
+        tmp = pd.read_json(file_name)
+        logger.info(f"Merged dataset: {tmp.shape}")
+        merged_dataset = tmp if dataset.empty else dataset.merge(tmp, how='outer')
     else:
         raise UnsupportedFileType("Detected unsupported file type")
     
@@ -50,6 +72,7 @@ def ingest_directory(directory_name: str, dataset: pd.DataFrame = pd.DataFrame()
             try:    
                 logger.info("File detected")
                 dataset = merge_dataset(item, dataset)
+                #dataset = append_dataset(item, dataset)
                 file_list.append(os.path.join(directory_name,item)) 
                 logger.info(f"Data appended, new dataset size: {dataset.shape}")
             except UnsupportedFileType as err:
@@ -66,14 +89,14 @@ def ingest_directory(directory_name: str, dataset: pd.DataFrame = pd.DataFrame()
 def clean_dataset(dataset: pd.DataFrame):
     
     # drop all duplicated records
-    logger.info(f"Dataset: {dataset}")
+    logger.info(f"Cleaning dataset size: {dataset.shape}")
     if (dataset.duplicated().any()):
         logger.info(f"Identified duplicated records in the merged dataset (current size: {dataset.shape}). Keeping the first occurance.")
         dataset.drop_duplicates(keep='first',inplace = True)
         dataset.reset_index(drop=True, inplace=True)
         logger.info(f"Duplicated records removed. New size: {dataset.shape}")
     
-    logger.info("clean_dataset() done.")
+    logger.info(f"Cleaned dataset size: {dataset.shape}")
 
 
 def save_dataset(
@@ -86,14 +109,17 @@ def save_dataset(
     dataset.to_csv(cleaned_pth, index=False)
 
     # write the merged files to file for further reference
-    #ast.file_list
+    with open(os.path.join(directory_name, ingested_file_name), 'w') as f:
+        for item in file_list:
+            f.write("%s\n" % item)
+
 
 def merge_multiple_dataframe():
     
     # Merging the data 
-    initial_wd = os.getcwd()
-    merged_dataset, merged_files = ingest_directory(os.path.join(initial_wd, input_folder_path))
-    logger.info(f"Initial working directory: {initial_wd} and folder {input_folder_path} are merged.")
+    working_dir = "" #os.getcwd()
+    merged_dataset, merged_files = ingest_directory(os.path.join(working_dir, input_folder_path))
+    logger.info(f"Initial working directory: {working_dir} and folder {input_folder_path} are merged.")
     logger.info(f"Merged files: {merged_files}")
 
     # Cleaning the merged dataset            
@@ -102,7 +128,7 @@ def merge_multiple_dataframe():
     # Save the cleand dataaset
     save_dataset(
         merged_dataset, merged_files,
-        os.path.join(initial_wd, output_folder_path),
+        os.path.join(working_dir, output_folder_path),
         "finaldata.csv", "ingestedfiles.txt"
     )
 
@@ -111,4 +137,4 @@ if __name__ == '__main__':
     try:
         merge_multiple_dataframe()
     except Exception as err:
-        print(f"Ingestion Error: {err}")
+        print(f"Ingestion Main Error: {err}")
--- training.py
+++ training.py
@@ -8,13 +8,22 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
 
+# initialize logging
+import logging 
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+# Exception handling
+
+
 ###################Load config.json and get path variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'], "finaldata.csv") 
 model_path = os.path.join(config['output_model_path']) 
-
+logger.info(f"dataset_pth: {dataset_csv_path}")
+logger.info(f"model_pth: {model_path}")
 
 #################Function for training the model
 def train_model():
@@ -30,3 +39,9 @@ def train_model():
     
     #write the trained model to your workspace in a file called trainedmodel.pkl
 
+
+if __name__ == '__main__':
+    try:
+        train_model()
+    except Exception as err:
+        print(f"Training Main Error: {err}")
